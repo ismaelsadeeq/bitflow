cc999d3cdd2c1c5759c64e01cb892c70cee6020c
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2022-11-15 21:38:36 +0100

    responseHandler

--- bitflow-server/src/response-handler/response-handler.service.ts
+++ bitflow-server/src/response-handler/response-handler.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ResponseHandlerService {}
+export class ResponseHandlerService {
+  data;
+  status:boolean;
+  message:string;
+  public static instance:ResponseHandlerService = new ResponseHandlerService();
+
+  constructor(){
+    this.data = {}
+  }
+
+  public responseBody(status:boolean,message:string,data){
+    if(data) this.data = data;
+    this.status = status;
+    this.message = message;
+    return {status:this.status,message:this.message,data:this.data}
+  }
+}
